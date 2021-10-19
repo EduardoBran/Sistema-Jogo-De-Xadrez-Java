722b26931cd25f70f21c68ae188ca92b19bf8262
Eduardo Brandao <eduardo.ads1814@gmail.com>
2021-10-19 11:13:41 -0300

    Implementing possible moves of King (Rei)

--- src/chess/pieces/Rei.java
+++ src/chess/pieces/Rei.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,11 +16,84 @@ public class Rei extends ChessPiece{
 	public String toString() {
 		return "R";
 	}
+	
+	private boolean canMove(Position position) {
+		
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0 , 0);
+		
+		//acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//baixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		//esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}		
+		
+		//direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Noroeste (superior esqurda)
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}		
+
+		//Nordeste (superior direito)
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Sudoeste (inferior esquerdo)
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Sudeste (inferior direito)
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		
+		if (getBoard().positionExists(p) && canMove(p)) {
+			
+			mat[p.getRow()][p.getColumn()] = true;
+		}		
+		
 		return mat;
 	}
 }
