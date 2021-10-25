f4143b49ff3f251f459de881ce3c2b50f3e59c14
Eduardo Brandao <eduardo.ads1814@gmail.com>
2021-10-25 14:44:53 -0300

    Bispo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bispo;
 import chess.pieces.Peao;
 import chess.pieces.Rei;
 import chess.pieces.Torre;
@@ -242,7 +243,9 @@ public class ChessMatch {
 	private void initialSetup() {
 		
 		placeNewPiece('a', 1, new Torre(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bispo(board, Color.WHITE));
         placeNewPiece('e', 1, new Rei(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bispo(board, Color.WHITE));
         placeNewPiece('h', 1, new Torre(board, Color.WHITE));
         placeNewPiece('a', 2, new Peao(board, Color.WHITE));
         placeNewPiece('b', 2, new Peao(board, Color.WHITE));
@@ -255,7 +258,9 @@ public class ChessMatch {
         
 
 		placeNewPiece('a', 8, new Torre(board, Color.BLACK));
+		placeNewPiece('c', 8, new Bispo(board, Color.BLACK));
         placeNewPiece('e', 8, new Rei(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bispo(board, Color.BLACK));
         placeNewPiece('h', 8, new Torre(board, Color.BLACK));
         placeNewPiece('a', 7, new Peao(board, Color.BLACK));
         placeNewPiece('b', 7, new Peao(board, Color.BLACK));
